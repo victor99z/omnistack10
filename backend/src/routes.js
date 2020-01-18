ba51cdf76059ac28b4e853c21ef51abd4c6abef8
Victor Bernardes <victor_luizbernardes5@outlook.com>
2020-01-18 17:38:52 -0300

    removing node_modules

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,28 @@
 const { Router } = require('express');
 const axios = require('axios');
+const Dev = require('./models/Dev.js');
 
 const routes = Router();
 
 routes.post('/devs', async (req , res) => {
-    const {github_username} = req.body;
+    const {github_username, techs } = req.body;
+
+    Dev.find({ github_username});
 
     const apiResponse = await axios.get(`https://api.github.com/users/${ github_username }`);
-
     const { name = login, avatar_url, bio} = apiResponse.data;
 
-    console.log(avatar_url+"\n"+bio) 
+    const techsArray = techs.split(',').map( tech => tech.trim());
 
-    return res.json({ message: "works" });
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray // sem short sytax do javascript
+    });
+
+    return res.json(dev);
 });
 
 module.exports = routes;
